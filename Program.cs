4e6ec9ac3af6d055006532e5864ed04f1a46b2ae
Elpulgo <oscar.f.larsson@gmail.com>
2021-12-04 21:11:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent for you!");
+            var day = new Day4.Day4();
+            day.Execute();
+        }
+
+
+    }
+}
