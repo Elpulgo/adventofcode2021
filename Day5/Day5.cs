4b69f92e5eae4315e56944e94b24a72f5a626e85
Oscar <oscar.f.larsson@gmail.com>
2021-12-05 20:56:36 +0100

    WIP Day 5 Part 2

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -10,99 +11,107 @@ namespace Advent.Day5
         internal void Execute()
         {
             Console.WriteLine("Day 5:");
-Part1();
-//             var test = @"
-//             0,9 -> 5,9
-// 8,0 -> 0,8
-// 9,4 -> 3,4
-// 2,2 -> 2,1
-// 7,0 -> 7,4
-// 6,4 -> 2,0
-// 0,9 -> 2,9
-// 3,4 -> 1,4
-// 0,0 -> 8,8
-// 5,5 -> 8,2";
+            Part1();
+            var test = @"
+            0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
 
-//             // var input = File
-//             //     .ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Day5", "Day5.txt"))
-//             var input = test.Split("\n", StringSplitOptions.RemoveEmptyEntries)
-//                 .ToList()
-//                 .Select(line =>
-//                 {
-//                     var coordinates = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
+            // var input = File
+            //     .ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Day5", "Day5.txt"))
+            var input = test.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .Select(line =>
+                {
+                    var coordinates = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
 
-//                     var firstCoords = coordinates.First().Split(",", StringSplitOptions.RemoveEmptyEntries);
-//                     var lastCoords = coordinates.Last().Split(",", StringSplitOptions.RemoveEmptyEntries);
+                    var firstCoords = coordinates.First().Split(",", StringSplitOptions.RemoveEmptyEntries);
+                    var lastCoords = coordinates.Last().Split(",", StringSplitOptions.RemoveEmptyEntries);
 
-//                     var first = (
-//                         x: Convert.ToInt32(firstCoords.First().Trim()),
-//                         y: Convert.ToInt32(firstCoords.Last().Trim())
-//                     );
+                    var first = (
+                        x: Convert.ToInt32(firstCoords.First().Trim()),
+                        y: Convert.ToInt32(firstCoords.Last().Trim())
+                    );
 
-//                     var last = (
-//                         x: Convert.ToInt32(lastCoords.First().Trim()),
-//                         y: Convert.ToInt32(lastCoords.Last().Trim())
-//                     );
+                    var last = (
+                        x: Convert.ToInt32(lastCoords.First().Trim()),
+                        y: Convert.ToInt32(lastCoords.Last().Trim())
+                    );
 
-//                     return (first, last);
-//                 }
-//                 )
-//                 // .Where(w => w.first.x == w.last.x || w.first.y == w.last.y)  
-//                 .Select(s =>
-//                     s.first.x == s.last.x
-//                     ?
-//                         s.first.y == new List<int>(){
-//                             s.first.y,
-//                             s.last.y
-//                         }.Min()
-//                             ? (s.first, s.last)
-//                             : (s.last, s.first)
-//                     :
-//                         s.first.x == new List<int>() {
-//                             s.first.x,
-//                             s.last.x
-//                         }.Min()
-//                         ? (s.first, s.last)
-//                         : (s.last, s.first)
-//                 )
-//                 .Select(s => (first: s.Item1, last: s.Item2))
-//                 .ToList();
+                    return (first, last);
+                }
+                )
+                // .Where(w => w.first.x == w.last.x || w.first.y == w.last.y)  
+                // .Select(s =>
+                //     s.first.x == s.last.x
+                //     ?
+                //         s.first.y == new List<int>(){
+                //             s.first.y,
+                //             s.last.y
+                //         }.Min()
+                //             ? (s.first, s.last)
+                //             : (s.last, s.first)
+                //     :
+                //         s.first.x == new List<int>() {
+                //             s.first.x,
+                //             s.last.x
+                //         }.Min()
+                //         ? (s.first, s.last)
+                //         : (s.last, s.first)
+                // )
+                // .Select(s => (first: s.Item1, last: s.Item2))
+                .ToList();
 
-//             var coords = new Dictionary<(int, int), int>();
+            var coords = new Dictionary<(int, int), int>();
 
-//             foreach (var line in input)
-//             {
-//                 if (line.first.x == line.last.x)
-//                 {
-//                     for(var y = line.first.y; y <= line.last.y; y++)
-//                     {
-//                         if(coords.ContainsKey((line.first.x, y))){
-//                             coords[(line.first.x, y)]++;
-//                             continue;
-//                         }
+            foreach (var line in input)
+            {
+                var positive = line.last.y - line.first.y == line.last.x - line.first.x;
+                var negative = line.last.y - line.first.y == line.first.x - line.last.x;
 
-//                         coords[(line.first.x, y)] = 1;
-//                     }
+                Console.WriteLine($"{line.first.x}, {line.first.y} -> {line.last.x}, {line.last.y} # pos: {(positive ? "/" : "")} neg: {(negative ? "\\" : "")}");
 
-//                     continue;
-//                 }
+// 8,0 -> 0,8  /
+// 6,4 -> 2,0  \
 
-//                 for(var x = line.first.x; x <= line.last.x; x++)
-//                 {
-//                     if(coords.ContainsKey((x, line.first.y))){
-//                         coords[(x, line.first.y)]++; 
-//                         continue;
-//                     }
+                if (line.first.x == line.last.x)
+                {
+                    for(var y = line.first.y; y <= line.last.y; y++)
+                    {
+                        if(coords.ContainsKey((line.first.x, y))){
+                            coords[(line.first.x, y)]++;
+                            continue;
+                        }
 
-//                     coords[(x, line.first.y)] = 1;
-//                 }
-//             }
+                        coords[(line.first.x, y)] = 1;
+                    }
 
-//             bool IsLinear((int x, int y) first, (int x, int y) last)
-//                 => first.x == last.x || first.y == last.y;
+                    continue;
+                }
+
+                for(var x = line.first.x; x <= line.last.x; x++)
+                {
+                    if(coords.ContainsKey((x, line.first.y))){
+                        coords[(x, line.first.y)]++; 
+                        continue;
+                    }
+
+                    coords[(x, line.first.y)] = 1;
+                }
+            }
+
+            bool IsLinear((int x, int y) first, (int x, int y) last)
+                => first.x == last.x || first.y == last.y;
             
 
-//             Console.WriteLine($"Part 1: {coords.Where(w => w.Value > 1).Count()}");
+            Console.WriteLine($"Part 1: {coords.Where(w => w.Value > 1).Count()}");
         }
 
         private void Part1(){
@@ -154,33 +163,30 @@ Part1();
 
             foreach (var line in input)
             {
-                if (line.first.x == line.last.x)
-                {
-                    for(var y = line.first.y; y <= line.last.y; y++)
-                    {
-                        if(coords.ContainsKey((line.first.x, y))){
-                            coords[(line.first.x, y)]++;
-                            continue;
-                        }
+                 var (first, last, opposite, xOrY) = line.first.x == line.last.x
+                    ? (line.first.y, line.last.y, line.first.x, 0)
+                    : (line.first.x, line.last.x, line.first.y, 1);
 
-                        coords[(line.first.x, y)] = 1;
-                    }
-
-                    continue;
-                }
-
-                for(var x = line.first.x; x <= line.last.x; x++)
-                {
-                    if(coords.ContainsKey((x, line.first.y))){
-                        coords[(x, line.first.y)]++; 
-                        continue;
-                    }
-
-                    coords[(x, line.first.y)] = 1;
-                }
+                Enumerable.Range(first, last - first + 1)
+                    .ToList()
+                    .ForEach(c => {
+                        (int x, int y) = xOrY == 0 ? (opposite, c) : (c, opposite);
+                        coords.AddOrUpdate(x, y);
+                    });
             }
 
-            Console.WriteLine($"Part 1: {coords.Where(w => w.Value > 1).Count()}");
+            Console.WriteLine($"Part 1: {coords.Count(c => c.Value > 1)}");
+        }
+    }
+
+    public static class DictionaryExtension{
+        public static void AddOrUpdate(this Dictionary<(int, int), int> dictionary, int x, int y){
+            if(dictionary.ContainsKey((x, y))){
+                dictionary[(x, y)]++; 
+                return;
+            }
+
+            dictionary[(x, y)] = 1;
         }
     }
 }
