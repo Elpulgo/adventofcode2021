79eb3888ff6d79719ad300484a55c4d4aba4a211
Elpulgo <oscar.f.larsson@gmail.com>
2021-12-07 10:05:20 +0100

    Updated with a more clever solution

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace adventofcode2021.Day7
@@ -21,14 +21,22 @@ namespace adventofcode2021.Day7
                 .Select(s => Convert.ToInt32(s))
                 .ToList();
 
+            var median = input[input.Count / 2];
+            var avg = input.Sum() / input.Count;
+
             var min = input.Min();
             var max = input.Max();
 
             var possibleValues = Enumerable.Range(min, (max - min) + 1)
                 .ToDictionary(d => d, d => 0);
 
+
             for (int i = min; i <= max; i++)
             {
+                // More clever solution using algebra..
+                //var delta = i - min;
+                //possibleValues[i] = input.Sum(s => (Math.Abs(s - delta) * (Math.Abs(s - delta) + 1)) / 2);
+
                 foreach (var inp in input)
                 {
                     var steps = Math.Abs(inp - i);
