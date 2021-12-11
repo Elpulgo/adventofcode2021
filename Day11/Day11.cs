9a3192b9de2173e6260bb18e466f57cdf8842ee4
Oscar <oscar.f.larsson@gmail.com>
2021-12-11 20:45:01 +0100

    Day 11 completed

--- Day11/Day11.cs
+++ Day11/Day11.cs
@@ -1,59 +1,86 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Reflection.Metadata;
 
 namespace adventofcode2021.Day11
 {
     internal class Day11 : BaseDay
     {
-        private Dictionary<(int X, int Y), Coordinate> _matrix = new();
+        private Dictionary<(int X, int Y), int> _matrix = new();
 
         internal void Execute()
         {
+            Console.WriteLine(nameof(Day11));
+
             var lines = ReadInput(nameof(Day11)).Select(s => s.ToCharArray().Select(s => Convert.ToInt32(s - '0')).ToList()).ToList();
-            
+            var sw = Stopwatch.StartNew();
+
             for (var y = 0; y < lines.Count; y++)
             {
                 for (var x = 0; x < lines[y].Count; x++)
                 {
-                    _matrix.Add((x, y), new Coordinate { Y = y, X = x, CurrentValue =  lines[y][x]});
+                    _matrix.Add((x, y), lines[y][x]);
                 }
             }
 
-            // Setup all neighbours
-            _matrix.ToList().ForEach(f => f.Value.Neighbours = GetNeighbours(f.Key).Select(s => _matrix[s]).ToList());
-
             var totalFlash = 0;
-
+            var synchronizedFlashes = 0;
             var steps = 0;
-            while(steps < 100){
-               _matrix.ToList().ForEach(f => {
-                    totalFlash += f.Value.Increment(f => GetNeighbours(f), _matrix);
-                });
+            
+            while(steps < 1000)
+            {
+                var stepVisited = new HashSet<(int X, int Y)>();
+                
+                // First increment all
+                foreach (var coord in _matrix)
+                {
+                    var value = _matrix[coord.Key];
+                    _matrix[coord.Key] = value == 9 ? 0 : value + 1;  
+                }
 
+                do {
+                    // Check if anyone flash
+                    foreach (var coord in _matrix.Where(w => !stepVisited.Contains(w.Key) && w.Value == 0))
+                    {                        
+                        if(_matrix[coord.Key] != 0)
+                            continue;            
+
+                        stepVisited.Add(coord.Key);
+                        totalFlash++;
+
+                        // Increment neighbours
+                        foreach (var neighbour in GetNeighbours(coord.Key))
+                        {
+                            var value = _matrix[neighbour];
+                            if(value == 0)
+                                continue;
+
+                            _matrix[neighbour] =  value == 9 ? 0 : value + 1;
+                        }
+                    }
+                } while(_matrix.Any(w => !stepVisited.Contains(w.Key) && w.Value == 0));
 
                 steps++;
-                // var anyLeftToFlash = true;
-                // while(anyLeftToFlash){
-                //     var coordsToFlash = _matrix.Where(w => w.Value == 9).ToList();
-                //     totalFlash += coordsToFlash.Count;
-                //     // Find adjecent here
 
-                // }
+                if(steps == 100){
+                    Console.WriteLine($"Part 1 took {sw.ElapsedMilliseconds} ms"); 
+                    FirstSolution(totalFlash.ToString());
+                }
 
-
-                // Reset all which have 9 to 0 for this step
-                // _matrix.Where(w => w.Value == 9).ToList().ForEach(f => matrix[f.Key] = 0);
+                if(_matrix.All(a => a.Value == 0)){
+                    synchronizedFlashes = steps;
+                    break;
+                }
             }
 
-            
-            FirstSolution(totalFlash.ToString());
-            // SecondSolution(string.Empty);
+            Console.WriteLine($"Part 2 took {sw.ElapsedMilliseconds} ms"); 
+            SecondSolution(synchronizedFlashes.ToString());
+            sw.Stop();
         }
 
-        private IEnumerable<(int X, int Y)> GetNeighbours((int X, int Y) coord){
-            
+        private IEnumerable<(int X, int Y)> GetNeighbours((int X, int Y) coord) 
+        {
             // Top
             if(_matrix.ContainsKey((coord.X, coord.Y - 1)))
                 yield return (coord.X, coord.Y - 1);
@@ -85,43 +112,6 @@ namespace adventofcode2021.Day11
             // Diagonal Right Bottom
             if(_matrix.ContainsKey((coord.X + 1, coord.Y + 1)))
                 yield return (coord.X + 1, coord.Y + 1);
-        }
-    }
-
-    internal class Coordinate {
-        public int CurrentValue = 0;
-        public List<Coordinate> Neighbours = new();
-
-        public int X {get; set;}
-        public int Y {get; set;}
-
-        public int Increment(Func<(int, int), IEnumerable<(int, int)>> neighbourFunc, Dictionary<(int X, int Y), Coordinate> matrix){
-            if(CurrentValue != 0){
-                if(CurrentValue == 9){
-                    CurrentValue = 0;
-                    return 0;
-                }
-                CurrentValue++;
-                return 0;
-            }
-
-            if(CurrentValue == 0){
-                var scopedCount = 1;
-                neighbourFunc((X, Y)).ToList().ForEach(n => scopedCount += matrix[(n)].Increment(neighbourFunc, matrix));
-                CurrentValue = 0;
-                CurrentValue++;
-
-                return scopedCount;
-            }
-
-            if(CurrentValue == 9){
-                    CurrentValue = 0;
-                    return 0;
-            }
-            CurrentValue++;
-
-            return 0;
-
         }
     }
 }
