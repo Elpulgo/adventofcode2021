57822c7319c93b1eac5f4512faca519bbd7e1697
Oscar <oscar.f.larsson@gmail.com>
2021-12-18 21:41:32 +0100

    Day 17 part 1 complete

--- Day17/Day17.cs
+++ Day17/Day17.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace adventofcode2021.Day16
@@ -11,17 +12,21 @@ namespace adventofcode2021.Day16
     {
         public Day17(bool shouldPrint) : base(nameof(Day17), shouldPrint) { }
 
-        private string Regex = @"[0-9-]{1,3}";
+        private string Regex = @"[0-9-]{1,4}";
         
         private int xHigh = 0;
         private int xLow = 0;
-
         private int yHigh = 0;
         private int yLow = 0;
+
         public override void Execute()
         {
             var input = ReadInput().First();
+
+            base.StartTimerOne();
+
             var split = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
             var xHighToLow = new Regex(Regex).Matches(split.First()).Select(s => Convert.ToInt32(s.Value)).OrderByDescending(o => o).ToList();
             xHigh = xHighToLow.First();
             xLow = xHighToLow.Last();
@@ -30,139 +35,101 @@ namespace adventofcode2021.Day16
             yHigh = yHighToLow.First();
             yLow = yHighToLow.Last();
 
-            // var position = (0, 0);
-            var steps = 0;
-            // var xMove = 7;
-            // var yMove = 2;
-
-            // var stepsyHigh = 0 - yHigh;
-            // var stepsyLow = 0 - yLow;
-            // var stepsXLow = xLow / xMove;
-            // var stepsXGhigh = xHigh / xMove;
-
-            // var apa = t * (t - 1)
-            // startX = n, where n is the largest value where    n * (n + 1) / 2 < MinX
-            // 6 * (6 + 1) / 2 < 20  == 20.5
-            // 5 * (5 +1 ) / 2 < 20 == 15
-
-            var velocities = new List<(int Velocity, int StepsRequiredToMin)>();
-
-            foreach (var probableVX in Enumerable.Range(1, 100))
-            {
-                var formula = probableVX * (probableVX + 1) / 2;
-                if(formula > xLow && formula < xHigh){
-                    velocities.Add((probableVX, StepsRequired(probableVX, xLow)));
-                }
-            } 
-
-            int StepsRequired(int velocity, int target){
-                int steps = 0;
-                while(target >= 0){
-                    target -= velocity;
-                    velocity--;
-                    steps++;
-                }
-
-                return steps;
-            }
+            var velocities = Enumerable.Range(1, 100).Where(probableVX => 
+                    (probableVX * (probableVX + 1) / 2) > xLow && 
+                    (probableVX * (probableVX + 1) / 2) < xHigh)
+                .Select(s => (Velocity: s, StepsRequiredToMin: StepsRequired(s, xLow)))
+                .ToList();
 
             var maxY = 0;
 
             foreach (var velocity in velocities)
             {
-                for(var y = 1; y < 10; y++){
-                    var position = (0,0);
-                    var xMove = velocity.Velocity;
-                    var yMove = y;
+                // Brute force, ugly but true!
+                for(var y = 0; y < 10000; y++){
+                    var position = (X: 0, Y: 0);
+                    var velocityX = velocity.Velocity;
+                    var velocityY = y;
 
-                    if(y == 9 && velocity.Velocity == 6){
-                        var nisse = string.Empty;
-                    }
+                    var probeRoundMaxY = 0;
 
-                    var roundMaxY = 0;
+                    while(!IsWithinTarget(position)){
+                        (velocityX, velocityY) = Step(ref position, velocityX, velocityY);
 
-                    while(!IsBeyondTarget(position, xMove)){
-                        (xMove, yMove) = Step(ref position, xMove, yMove);
-
-                        // if(IsBeyondTarget(position, xMove)){
-                        //     roundMaxY = 0;
-                        //     break;
-                        // }
-
-                        if(IsWithinTarget(position)){
-                            if(position.Item2 > maxY){
-                                maxY = position.Item2;
-                                break;
-                            }
+                        if(IsBeyondTarget(position, velocityX)){
+                            probeRoundMaxY = 0;
+                            break;
                         }
-                        // Console.WriteLine(position);
-                        // if(position.Item2 > roundMaxY){
-                        //     maxY = position.Item2;
 
-                        //     roundMaxY = position.Item2;
-                        // }
+                        if(position.Y > probeRoundMaxY){
+                            probeRoundMaxY = position.Y;
+                        }
                     }
 
-                    // if(roundMaxY > maxY){
-                    //     roundMaxY = maxY;
-                    // }
+                    if(probeRoundMaxY > maxY){
+                        maxY = probeRoundMaxY;
+                    }
                 }
+            }          
+
+            base.StopTimerOne();
+            FirstSolution(maxY.ToString());
+        }
+
+         private int StepsRequired(int velocity, int target){
+            int steps = 0;
+            while(target >= 0){
+                target -= velocity;
+                velocity--;
+                steps++;
             }
 
-            var apa122312 = string.Empty;
-
-
-
-            // while (!IsWithinTarget(position))
-            // {
-            //     (xMove, yMove) = Step(ref position, xMove, yMove);
-            //     steps++;
-            // }
-
-            // 0 - yHigh - x == How many steps will it need till we het yHigh?
-            // 0 - yLow - x == How many steps will it need till we het yLow?
-
-
-            // The probe's x position increases by its x velocity.
-            // The probe's y position increases by its y velocity.
-            // Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
-            // Due to gravity, the probe's y velocity decreases by 1.
+            return steps;
         }
 
         private bool IsWithinTarget((int X, int Y) position)
         {
-            if ((position.X <= xHigh && position.X >= xLow) && (position.Y <= yHigh && position.Y >= yLow))
+            if ((position.X <= xHigh && position.X >= xLow) && 
+                (position.Y <= yHigh && position.Y >= yLow)){
+                    if(position.Y == 99){
+                        var apa = string.Empty;
+                    }
                 return true;
+                }
 
             return false;
         }
 
-        private bool IsBeyondTarget((int X, int Y) position, int velocity)
+        private bool IsBeyondTarget((int X, int Y) position, int velocityX)
         {
             if(position.Y < yLow)
                 return true;
 
-            if(position.X > xHigh || (position.X < xLow && velocity == 0))
+            if(position.X > xHigh || (position.X < xLow && velocityX == 0))
                 return true;
 
             return false;
         }
 
-        private (int xMove, int yMove) Step(ref (int X, int Y) position, int xMove, int yMove)
+        // The probe's x position increases by its x velocity.
+        // The probe's y position increases by its y velocity.
+        // Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
+        // Due to gravity, the probe's y velocity decreases by 1.
+        private (int velocityX, int velocityY) Step(ref (int X, int Y) position, int velocityX, int velocityY)
         {
-            position.X += xMove;
-            position.Y += yMove;
+            position.X += velocityX;
+            position.Y += velocityY;
 
-            xMove = xMove switch
+            velocityX = velocityX switch
             {
-                > 0 => xMove -= 1,
-                < 0 => xMove += 1,
-                _ => xMove
+                > 0 => velocityX -= 1,
+                < 0 => velocityX += 1,
+                _ => velocityX
             };
 
-            yMove--;
+            velocityY--;
 
-            return (xMove, yMove);
+            return (velocityX, velocityY);
         }
     }
 }
