5cb8c790530805757d98d5fb13a5d7ad8f7fd73f
Elpulgo <oscar.f.larsson@gmail.com>
2021-12-21 20:15:38 +0100

    Update Dijkstra.cs

--- Helpers/Dijkstra.cs
+++ Helpers/Dijkstra.cs
@@ -4,15 +4,17 @@ using System.Linq;
 
 namespace adventofcode2021.Helpers
 {
-    public class Dijkstra
+    public static class Dijkstra
     {
         public static (Dictionary<TState, int>, TState) RunDijkstra<TState>(
-		TState start,
-		Func<TState, IEnumerable<(TState state, int cost)>> getNextStates,
-		Func<Dictionary<TState, int>, TState, bool> endingCondition) where TState : DijkstraNode, IComparable<TState>
-	    {
+        TState start,
+        Func<TState, IEnumerable<(TState state, int cost)>> getNextStates,
+        Func<Dictionary<TState, int>, TState, bool> endingCondition) where TState : DijkstraNode, IComparable<TState>
+        {
             var totalCost = new Dictionary<TState, int>();
+            var seen = new HashSet<(int, int)>();
             var pq = new PriorityQueue<TState>();
+
             pq.Enqueue(start);
 
             TState p = default;
@@ -21,7 +23,8 @@ namespace adventofcode2021.Helpers
                 p = pq.Dequeue();
                 var cost = p.Cost;
 
-                while (pq.Count != 0 && totalCost.ContainsKey(p)){
+                while (pq.Count != 0 && totalCost.ContainsKey(p))
+                {
                     p = pq.Dequeue();
                     cost = p.Cost;
                 }
@@ -31,19 +34,17 @@ namespace adventofcode2021.Helpers
                     break;
 
                 var states = getNextStates(p)
-                    .Select(s => (s.state, cost + s.cost));
+                    .Select(s => (State: s.state, Cost: cost + s.state.Cost));
 
-                foreach (var st in states)
-                {
-                    pq.Enqueue(st.state);
-                }
+                foreach (var st in states.Where(w => !seen.Contains((w.State.X, w.State.Y))))
+                    pq.Enqueue(st.State with { Cost = st.Cost });
+
+                seen.Add((p.X, p.Y));
             }
 
-		    return (totalCost, p);
-	    }
+            return (totalCost, p);
+        }
     }
 
-    public class DijkstraNode{
-        public int Cost { get; set; }
-    }
+    public record DijkstraNode(int X, int Y, int Cost);
 }
